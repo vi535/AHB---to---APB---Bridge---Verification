e19401c3ffd91846816bd6a95e45daf3df4f2dfa
vi535 <130845004+vi535@users.noreply.github.com>
2025-04-22 17:10:14 +0530

    Add files via upload

---
+++ AHB_driver.sv
@@ -0,0 +1,93 @@
+//AHB_Driver
+
+class ahb_driver extends uvm_driver #(ahb_xtn);
+	
+	//factory registration
+	`uvm_component_utils (ahb_driver)
+	
+	//interface
+	virtual ahb_if.AHB_DR_MP vif;
+	
+	ahb_xtn xtn;
+
+	ahb_agent_config ahb_cfg;
+	
+	extern function new (string name = "ahb_driver", uvm_component parent);
+	extern function void build_phase (uvm_phase phase);
+	extern function void connect_phase (uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+	extern task send_to_dut (ahb_xtn xtn);
+endclass: ahb_driver
+
+//-----------Constructor----------//
+function ahb_driver::new (string name = "ahb_driver", uvm_component parent);
+	super.new (name, parent);
+endfunction: new
+
+//-----------Build Phase---------//
+function void ahb_driver::build_phase (uvm_phase phase);
+	super.build_phase(phase);
+	
+	if(!uvm_config_db #(ahb_agent_config)::get(this, "", "ahb_agent_config", ahb_cfg))
+	//m_cfg is the local handle through which we want to read the variable from config db
+	//first 3 parameters of get method gives the address 
+		`uvm_fatal("CONFIG","Cannot get() m_cfg from uvm_config_db. Have you set it?")
+endfunction: build_phase
+
+
+function void ahb_driver::connect_phase (uvm_phase phase);
+	super.connect_phase(phase);
+	
+	//interface connection
+	vif=ahb_cfg.vif;
+	
+endfunction: connect_phase
+
+//////---Run Phase-----/////
+task ahb_driver::run_phase(uvm_phase phase);
+
+        //Active low reset
+        @(vif.ahb_drv_cb);
+        vif.ahb_drv_cb.Hresetn <= 1'b0;
+
+        repeat(3)
+        @(vif.ahb_drv_cb);
+        vif.ahb_drv_cb.Hresetn <= 1'b1;
+
+        forever
+                begin
+                        seq_item_port.get_next_item(req); //get the data from sequencer
+                        req.print();
+                        send_to_dut(req);
+                        seq_item_port.item_done();
+                end
+endtask
+
+
+///----send to dut------/////
+task ahb_driver::send_to_dut(ahb_xtn xtn);
+
+        //drive addr and control info
+        vif.ahb_drv_cb.Hwrite  <= xtn.Hwrite;
+        vif.ahb_drv_cb.Htrans <= xtn.Htrans;
+        vif.ahb_drv_cb.Hsize   <= xtn.Hsize;
+        vif.ahb_drv_cb.Haddr   <= xtn.Haddr;
+        vif.ahb_drv_cb.Hreadyin<= 1'b1;
+		
+        @(vif.ahb_drv_cb);
+
+        //wait till Hreadyout goes high - the moment it goes high drive Hwdata
+        wait(vif.ahb_drv_cb.Hreadyout)
+                vif.ahb_drv_cb.Hwdata<=xtn.Hwdata;
+
+        //`uvm_info("AHB_DRIVER", "Displaying ahb_driver data", UVM_LOW)
+        //xtn.print();
+
+        //After driving Hwdata, we should immediately drive the address in the same cycle, so endtask w/o any delay
+
+        ahb_cfg.drv_data_count++;
+	$display("No of packets driven = %0d", ahb_cfg.drv_data_count);
+endtask
+
+/*In AHB_DRIVER, we will declare the local interface handle and assign it to the interface defined in AHB_CONFIG in the connect phase.
+First, get the data from the Sequencer and drive this data to the DUT (Bridge) via the Interface.*/
---
+++ AHB_seqs.sv
@@ -0,0 +1,334 @@
+class base_sequence_ahb extends uvm_sequence #(ahb_xtn);
+
+        `uvm_object_utils(base_sequence_ahb)
+             
+	        logic [31:0] haddr;
+                logic hwrite;
+                logic [2:0] hsize;
+                logic [2:0] hburst;
+	
+        extern function new(string name = "base_sequence_ahb");
+endclass
+
+function base_sequence_ahb::new(string name = "base_sequence_ahb");
+        super.new(name);
+endfunction
+
+//back to back write//
+class write_back extends base_sequence_ahb;
+
+	`uvm_object_utils(write_back)
+
+	
+                logic [31:0] haddr1;
+                logic hwrite1;
+                logic [2:0] hsize1;
+                logic [2:0] hburst1;
+
+
+	extern function new(string name = "write_back");
+        extern task body();
+endclass
+
+function write_back::new(string name = "write_back");
+        super.new(name);
+endfunction
+
+
+task write_back::body();
+
+        repeat(15)
+        begin
+        req = ahb_xtn::type_id::create("req");
+
+        start_item(req);
+        assert(req.randomize() with {Htrans == 2'b10 && Hburst == 3'b000 && Hwdata inside {[0:32'h0000_ffff]};}); //first xtn is NS
+        finish_item(req);
+
+        end
+
+	//store in local variables
+        haddr = req.Haddr;
+        hsize = req.Hsize;
+        hburst = req.Hburst;
+        hwrite = req.Hwrite;
+
+        //-----UNSPECIFIED LENGTH---------//
+        if(hburst == 3'b001)
+        begin
+                for(int i=0; i<req.length-1; i++)
+                begin
+                        //haddr = haddr + (2**hsize);
+
+                        start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 1'b1;});
+
+                        if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 2'b10;});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 3'b100;});
+
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+                end
+        end    
+
+	start_item(req);
+        assert(req.randomize() with {Htrans == 2'b00;}); //first xtn is NS
+        finish_item(req);
+                                                                   
+endtask: body
+
+///-------test sequence-------/////
+class test_sequence extends base_sequence_ahb;
+
+        `uvm_object_utils(test_sequence)
+
+               // logic [31:0] haddr;
+               // logic hwrite;
+                //logic [2:0] hsize;
+               // logic [2:0] hburst;
+
+        extern function new(string name = "test_sequence");
+        extern task body();
+endclass
+
+function test_sequence::new(string name = "test_sequence");
+        super.new(name);
+endfunction
+
+task test_sequence::body();
+	repeat(10)
+	begin
+        req = ahb_xtn::type_id::create("req");
+
+        start_item(req);
+	assert(req.randomize() with {Htrans == 2'b10 && Hburst inside {[1:7]} && Hwdata inside {[32'h0001_ffff:32'hffff_ffff]};}); //first xtn is NS
+	finish_item(req);
+
+        //store in local variables
+        haddr = req.Haddr;
+        hsize = req.Hsize;
+        hburst = req.Hburst;
+        hwrite = req.Hwrite;
+
+        //-----INCR 4   ---------//
+        if(hburst == 3'b011)
+        begin
+                for(int i=0; i<3; i++)
+                begin
+                        //haddr = haddr + (2**hsize);
+                        start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 1'b1;});
+
+                        if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 2'b10;});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                 	Haddr == haddr +3'b100;});
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+                end
+        end
+
+        //-----INCR 8---------//
+        if(hburst == 3'b101)
+        begin
+                for(int i=0; i<7; i++)
+                begin
+                        //haddr = haddr + (2**hsize);
+
+                        start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 1'b1;});
+
+                        if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 2'b10;});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 3'b100;});
+
+                        finish_item(req);
+ 			haddr = req.Haddr;
+                end
+        end
+
+        //-----INCR 16---------//
+        if(hburst == 3'b111)
+        begin
+                for(int i=0; i<15; i++)
+                begin
+                        //haddr = haddr + (2**hsize);
+
+                        start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 1'b1;});
+
+                        if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 2'b10;});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 3'b100;});
+
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+                end
+        end
+//-----INCR unspecified length---------//
+        if(hburst == 3'b001)
+        begin
+                for(int i=0; i<req.length-1; i++)
+                begin
+                        //haddr = haddr + (2**hsize);
+
+                        start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 1'b1;});
+
+                        if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 2'b10;});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == haddr + 3'b100;});
+
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+                end
+        end
+
+        //----WRAP 4-----///
+        if(hburst == 3'b010)
+        begin
+                for(int i=0; i<3; i++)
+                begin
+			start_item(req);
+
+			if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:2], haddr[1:0] + 1'b1};});
+
+                        if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:3], haddr [2:1]+ 1'b1, haddr[0]};});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:4], haddr[3:2] + 1'b1, haddr[1:0]};});
+
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+        end
+        end
+
+        //----WRAP 8-----///
+        if(hburst == 3'b100)
+        begin
+                for(int i=0; i<7; i++)
+                begin
+			start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:3], haddr[2:0] + 1'b1};});
+			if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:4], haddr[3:1] + 1'b1, haddr[0]};});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:5], haddr[4:2] + 1'b1, haddr[1:0]};});
+
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+        end
+        end
+
+        //----WRAP 16-----///
+        if(hburst == 3'b110)
+        begin
+                for(int i=0; i<15; i++)
+                begin
+			start_item(req);
+
+                        if(hsize == 0)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:4], haddr[3:0] + 1'b1};});
+
+			if(hsize == 1)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {haddr[31:5], haddr[4:1] + 1'b1, haddr[0]};});
+
+                        if(hsize == 2)
+                                assert(req.randomize() with {Hsize == hsize; Hburst == hburst;
+                                                                                        Hwrite == hwrite; Htrans == 2'b11;
+                                                                                        Haddr == {Haddr[31:6], haddr[5:2] + 1'b1, haddr[1:0]};});
+
+                        finish_item(req);
+
+                        haddr = req.Haddr;
+        end
+        end
+	end
+
+	
+	start_item(req);
+        assert(req.randomize() with {Htrans == 2'b00;}); //first xtn is NS
+	finish_item(req);
+	
+	
+endtask: body
+
+//In AHB_SEQUENCE we will first create a Non-sequential transaction which will be stored in local variables as this data won't change for the entire transfer of data. Later, 
+//depending on the randomized Hburst and Hsize values, SEQUENTIAL transactions will be generated. 
